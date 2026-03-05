85ca1bb2761847625a100cfa4810b79a5e8abe2d
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-03-05 17:35:17 +0100

    Create db.py

---
+++ db.py
@@ -0,0 +1,10 @@
+import os
+import psycopg2
+import psycopg2.extras
+
+def get_conn():
+    return psycopg2.connect(
+        os.environ["DATABASE_URL"],
+        sslmode="require",
+        cursor_factory=psycopg2.extras.DictCursor
+    )
